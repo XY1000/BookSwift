2e2e20a2c061dc69bda3d01c294b24ac505da4ef
huxueyuan <huxueyuan@mirror-networks.com>
2016-04-11 19:11:32 +0800

    no message

--- BookSwift/控制器/书城/BookShopTableController.swift
+++ BookSwift/控制器/书城/BookShopTableController.swift
@@ -9,7 +9,53 @@
 
 class BookShopTableController: UITableViewController {
 
+    lazy var bookshopVM : BookShopViewModel = {
+        
+        return BookShopViewModel()
+        
+    }()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.requestDataByNet()
+        
+    }
+    
+    func requestDataByNet() -> Void {
+        
+        
+        self.bookshopVM.getDataByNetComplete({ (isSuccess) -> Void in
+            
+            if isSuccess {
+                
+                self.tableView.reloadData()
+                
+            }else {
+                
+                
+                SVProgressHUD.showErrorWithStatus("后台维护中")
+            }
+            
+            
+            }) { (error) -> Void in
+                
+        }
+        
+    }
+    
+//MARK:UITableViewDelegate/DataSource
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 1;
+        
+    }
     
     
     
+
 }
+
+
--- BookSwift/控制器/书城/BookShopViewModel.swift
+++ BookSwift/控制器/书城/BookShopViewModel.swift
@@ -10,4 +10,66 @@
 
 class BookShopViewModel: BaseViewModel {
 
+    /*
+    \var agoSpecialColumn: [AgospecialModel]?
+    
+    var tailoredBook: [TailoredbookModel]?
+    
+    var hotBook: [HotbookModel]?
+    
+    var newGoodBook: [NewgoodbookModel]?
+    
+    var aloneBook: [AlonebookModel]?
+    
+    var newSpecialColumn: [NewspecialModel]?
+    */
+    private var headerArr: [NewspecialModel]?
+    private var bottomArr: [TailoredbookModel]?
+    private var aloneArr: [AlonebookModel]?
+    private var hotArr: [HotbookModel]?
+    private var xinArr: [NewgoodbookModel]?
+    private var liangArr: [AgospecialModel]?
+    
+    func getDataByNetComplete(success:(isSuccess:Bool)->Void,fail:(error:NSError)->Void)->Void {
+        
+        
+        NetManager.sharedManager().getBookShopData({ (Successback) -> Void in
+         
+            
+            
+            
+            
+            }) { (error) -> Void in
+                
+                
+                fail(error: error)
+                
+        }
+        
+        
+    }
+    
+//MARK: 头部数据
+    var headerNum : NSInteger {
+        
+        get {
+            
+          return (headerArr?.count)!
+            
+        }
+        
+        
+    }
+    
+//MARK: 量身打造
+
+
+//MARK: 新书推荐
+    
+//MARK: 热门欣赏
+
+//MARK: 独家首发
+    
+    
+//MARK: 底部视图
 }
