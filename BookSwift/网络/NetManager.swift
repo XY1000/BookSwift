376a5da36ce7a5af85040d7203e0de24502c9076
huxueyuan <huxueyuan@mirror-networks.com>
2016-04-27 09:07:30 +0800

    修改网络接口

--- BookSwift/网络/NetManager.swift
+++ BookSwift/网络/NetManager.swift
@@ -112,7 +112,7 @@ class NetManager: NSObject {
 //MARK: 书籍详情
     func getBookDetailData(pageFlg:String,bookid:String,userId:String,success:(Successback:BookDetailModel)-> Void,fail:(error:NSError)->Void) {
         
-        let url = (baseUrl as String) + "/BookReadPublished/bookInfo/getBookInfoCommentList?pageFlg=1&bookid=1&userId=1"
+        let url = (baseUrl as String) + "/BookReadPublished/bookInfo/getBookInfoCommentList?pageFlg=1&bookId=1&userId=1"
         
        // let param : [String:String] = ["pageFlg":pageFlg,"bookid":bookid,"userId":userId]
         
